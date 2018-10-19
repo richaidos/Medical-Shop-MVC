75f9865442d2782e0e46d3845414afadc7bd8de4
Aidos <aidossuper@gmail.com>
2018-10-19 23:18:22 +0600

    Добавьте файлы проекта.

---
+++ Medical-Shop-MVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Medical_Shop_MVC.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
